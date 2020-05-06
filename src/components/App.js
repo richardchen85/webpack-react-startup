8e4a45508f568561da32c38f2ce4060c8f5f5e26
陈礼强 <cdchenliqiang@jd.com>
2020-05-06 14:26:27 +0800

    update dependencies

--- src/components/App.js
+++ src/components/App.js
@@ -2,43 +2,6 @@ import React, { Component } from 'react';
 import HelloTs from './HelloTs';
 import styles from './App.less';
 
-function bind(target, key, descriptor) {
-  let fn = descriptor.value;
-  let definingProperty = false;
-
-  return {
-    configurable: true,
-    get() {
-      if (
-        definingProperty ||
-        this === target.prototype ||
-        this.hasOwnProperty(key) ||
-        typeof fn !== 'function'
-      ) {
-        return fn;
-      }
-
-      let boundFn = fn.bind(this);
-      definingProperty = true;
-      Object.defineProperty(this, key, {
-        configurable: true,
-        get() {
-          return boundFn;
-        },
-        set(value) {
-          fn = value;
-          delete this[key];
-        },
-      });
-      definingProperty = false;
-      return boundFn;
-    },
-    set(value) {
-      fn = value;
-    },
-  };
-}
-
 export default class App extends Component {
   state = {
     title: 'Webpack React Startup',
@@ -57,8 +20,7 @@ export default class App extends Component {
     );
   }
 
-  @bind
-  btnClick = e => {
+  btnClick = (e) => {
     alert(e.target.innerText);
   };
 }
